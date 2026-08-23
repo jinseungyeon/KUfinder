55ecf2ca0877093733a71903554931a851e3672f
jinseungyeon <141200736+jinseungyeon@users.noreply.github.com>
2026-08-23 13:14:41 +0900

    초기화

--- KUfinder/FE/src/pages/NotFoundPage.tsx
+++
@@ -1,5 +0,0 @@
-import { Link } from 'react-router-dom'
-
-export default function NotFoundPage() {
-  return <main className="content-wrap"><div className="card p-10 text-center"><h1 className="text-3xl font-black">페이지를 찾을 수 없습니다.</h1><Link className="btn-primary mt-5" to="/">홈으로</Link></div></main>
-}
